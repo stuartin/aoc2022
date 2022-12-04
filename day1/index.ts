92c185d364b1f74e9d770d2e6095e02bb1d3c735
stuartin <13344713+stuartin@users.noreply.github.com>
2022-12-04 15:21:41 +1100

    start day two

--- day1/index.ts
+++ day1/index.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from "fs";
 
-export function dayOne(): number[] {
+function partOne(): number[] {
     const input = readFileSync("./day1/data.txt").toString("utf-8").split("\r\n").map(string => parseInt(string))
     const elfs: number[] = []
 
@@ -15,3 +15,15 @@ export function dayOne(): number[] {
     console.log(`max: ${Math.max(...elfs)}`)
     return elfs
 }
+
+
+// Part Two
+const elfs = partOne()
+
+const sortedHighToLow = elfs.sort(function (a, b) {  return b - a;  })
+
+const topThree = sortedHighToLow.slice(0, 3)
+
+const total = topThree.reduce((acc, cur) => { return acc + cur }, 0)
+
+console.log(`total: ${total}`)
