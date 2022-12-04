4a21e7cf35162f706bfcd830fa9c4daedc5a441c
stuartin <13344713+stuartin@users.noreply.github.com>
2022-12-04 18:05:01 +1100

    day2 final

--- day2/index.ts
+++ day2/index.ts
@@ -7,15 +7,33 @@ const myAction: { [K in PTwo]: Action} = {
 }
 
 const theirAction: { [K in POne]: Action} = {
-    c: "scissors",
     a: "rock",
-    b: "paper"
+    b: "paper",
+    c: "scissors"
+}
+
+const toDrawAgainst: { [K in Action]: Action} = {
+    rock: "rock",
+    paper: "paper",
+    scissors: "scissors"
+}
+
+const toWinAgainst: { [K in Action]: Action} = {
+    rock: "paper",
+    paper: "scissors",
+    scissors: "rock"
+}
+
+const toLoseAgainst: { [K in Action]: Action} = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper"
 }
 
 const wasWin: { [K in Action]: POne} = {
-    "rock": "c",
-    "paper": "a",
-    "scissors": "b"
+    rock: "c",
+    paper: "a",
+    scissors: "b"
 }
 
 const wasDraw: { [K in Action]: POne} = {
@@ -24,16 +42,10 @@ const wasDraw: { [K in Action]: POne} = {
     scissors: "c"
 }
 
-const actionScore: {[K in PTwo]: number} = {
+const partOneActionScore: {[K in PTwo]: number} = {
     x: 1,
     y: 2,
     z: 3
-}
-
-const roundScore = {
-    lost: 0,
-    draw: 3,
-    win: 6
 }
 
 const didWin = (a: POne, b: PTwo) => {
@@ -53,13 +65,55 @@ const getRoundScore = (a: POne, b: PTwo) => {
 type POne = "a" | "b" | "c"
 type PTwo = "y" | "x" | "z"
 type Action = "rock" | "paper" | "scissors"
-type Result = "win" | "draw" | "lost"
+type Result = "lost" | "draw" | "win"
 type Outcome = {
-    didWin: boolean
+    didWin?: boolean
     actionScore: number
     roundScore: number
     totalScore: number
 }
+
+const partTwoActionScore: {[K in Action]: number} = {
+    rock: 1,
+    paper: 2,
+    scissors: 3
+}
+
+const roundScore: {[K in Result]: number} = {
+    lost: 0,
+    draw: 3,
+    win: 6
+}
+
+const desiredOutcome: {[K in PTwo]: Result} = {
+    x: "lost",
+    y: "draw",
+    z: "win"
+}
+
+const selectAction = (a: POne, desiredOutcome?: Result): Action => {
+    const theirChoice = theirAction[a]
+
+    if (desiredOutcome) {
+        switch (desiredOutcome) {
+            case "draw":
+                return toDrawAgainst[theirChoice]
+                break;
+
+            case "lost":
+                return toLoseAgainst[theirChoice]
+                break;
+
+            default:
+            case "win":
+                return toWinAgainst[theirChoice]
+                break;
+        }
+    }
+
+    return toWinAgainst[theirChoice]
+}
+
 
 const testData = [
     "A Y",
@@ -69,20 +123,46 @@ const testData = [
 
 const input = readFileSync("./day2/data.txt").toString("utf-8").split("\r\n")
 
-const results: Outcome[] = input.map(match => {
-    const pOne = match.split(" ")[0].toLowerCase() as POne 
-    const pTwo = match.split(" ")[1].toLowerCase() as PTwo
+const partOne = () => {
+    const results: Outcome[] = input.map(match => {
+        const pOne = match.split(" ")[0].toLowerCase() as POne 
+        const pTwo = match.split(" ")[1].toLowerCase() as PTwo
+    
+        return {
+            actionScore: partOneActionScore[pTwo],
+            roundScore: getRoundScore(pOne, pTwo),
+            totalScore: partOneActionScore[pTwo] + getRoundScore(pOne, pTwo)
+        }
+    })
+    
+    const totalScore = results.reduce((acc, curr) => {
+        return acc + curr.totalScore
+    }, 0)
+    
+    console.log(`part one: ${totalScore}`)
+}
 
-    return {
-        didWin: didWin(pOne, pTwo),
-        actionScore: actionScore[pTwo],
-        roundScore: getRoundScore(pOne, pTwo),
-        totalScore: actionScore[pTwo] + getRoundScore(pOne, pTwo)
-    }
-})
+const partTwo = () => {
+    const results: Outcome[] = input.map(match => {
+        const pOne = match.split(" ")[0].toLowerCase() as POne 
+        const pTwo = match.split(" ")[1].toLowerCase() as PTwo
 
-const totalScore = results.reduce((acc, curr) => {
-    return acc + curr.totalScore
-}, 0)
+        const myAction = selectAction(pOne, desiredOutcome[pTwo])
+    
+        const result =  {
+            actionScore: partTwoActionScore[myAction],
+            roundScore: roundScore[desiredOutcome[pTwo]],
+            totalScore: partTwoActionScore[myAction] + roundScore[desiredOutcome[pTwo]]
+        }
+        return result
+    })
+    
+    const totalScore = results.reduce((acc, curr) => {
+        return acc + curr.totalScore
+    }, 0)
+    
+    console.log(`part Two: ${totalScore}`)
+}
 
-console.log(totalScore)
+partOne()
+partTwo()
